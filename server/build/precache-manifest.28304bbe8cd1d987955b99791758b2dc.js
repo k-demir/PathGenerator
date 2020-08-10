7a4175bb8f754b353ac36baf33e9d832d44dd513
Kadir Demir <k0dee@hotmail.com>
2020-08-10 14:58:00 +0300

    changes for deployment to heroku

---
+++ server/build/precache-manifest.28304bbe8cd1d987955b99791758b2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c222ab833ce6df09c32397df9e43d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d259ef2f8207adf41f4c",
+    "url": "/static/css/main.df73695e.chunk.css"
+  },
+  {
+    "revision": "b5dbbdd48d3fd745f816",
+    "url": "/static/js/2.0ef78098.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/static/js/2.0ef78098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d259ef2f8207adf41f4c",
+    "url": "/static/js/main.6c028800.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static('build'))
 app.use(morgan('tiny'))
 
 app.use('/api/path', pathRouter)
